8a845bbfc7ab6a30e17e39637ece19df99a3dd0a
Natasha2702 <126653681+Natasha2702@users.noreply.github.com>
2025-05-09 21:30:01 +0200

    fixed the response font size

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePlot(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "e6taed4cb9ef5dfoaff8f35a499504ef";
   let context =
-    "You are the best at giving movie plots. You mission is to generate a 4 line plot using HTML but dont show the html word. Make sure to follow the user instructions and make it clean. Include a title to the movie. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are the best at giving movie plots. You mission is to generate a 4 line plot using HTML but dont show the html word. Make sure to follow the user instructions and make it clean. Always Include a title to the movie. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a movie plot on ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
